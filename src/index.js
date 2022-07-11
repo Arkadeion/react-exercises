44fb48a5f32d447d16051191f8de05be45adaea5
Arkadeion <arkadeion@gmail.com>
2022-07-11 11:45:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { createRoot } from "react-dom/client"
 
-const root = createRoot(
+const root = ReactDOM.createRoot(
     document.getElementById('root')
 );
 
