2c3d78556c77491d3b36346b948af38615b2e2d9
Arkadeion <arkadeion@gmail.com>
2022-07-11 12:11:13 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { HelloWorld } from "./HelloWorld";
+import { App } from "./App";
+import ReactDOM from "react-dom/client"
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <HelloWorld />
+        <App />
     </React.StrictMode>
 );
