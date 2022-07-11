404b206c4a9acc50c93f7806247224353475392a
Arkadeion <arkadeion@gmail.com>
2022-07-11 11:56:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { HelloWorld } from "./HelloWorld";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <App />
+        <HelloWorld />
     </React.StrictMode>
 );
