eeaca652114c0906efb280c8957aca7d489fb4ee
Arkadeion <arkadeion@gmail.com>
2022-07-13 16:23:10 +0200

    Fix Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
         return (
             <div>
                 <p>Welcome, {this.props.name}!</p>
-                <Age />
+                <Age age={this.props.age} />
             </div>
         )
     }
