da99c8c4693442c5a3da9701b8af4316039a5f3b
Arkadeion <arkadeion@gmail.com>
2022-07-23 23:08:32 +0200

    Fix typo

--- src/Login.js
+++ src/Login.js
@@ -22,9 +22,9 @@ export class Login extends React.Component {
     render() {
         return (
             <div>
-                <input name="username" value={this.state.value} onChange={this.handleInput} />
-                <input name="password" type='password' value={this.state.value} onChange={this.handleInput} />
-                <input name="checkbox" type='checkbox' value={this.state.value} onChange={this.handleInput} />
+                <input name="username" value={this.state.username} onChange={this.handleInput} />
+                <input name="password" type='password' value={this.state.password} onChange={this.handleInput} />
+                <input name="checkbox" type='checkbox' value={this.state.checkbox} onChange={this.handleInput} />
                 <button name="submit" type='submit' disabled={!this.state.username || !this.state.password} onClick={this.props._onLogin}>Login</button>
             </div>
         )
