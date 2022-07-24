76a981ccfec403c6fc608c71abfb69c8664d034e
Arkadeion <arkadeion@gmail.com>
2022-07-24 18:14:55 +0200

    Fix 4

--- src/Login.js
+++ src/Login.js
@@ -19,13 +19,20 @@ export class Login extends React.Component {
         })
     }
 
+    getFormData = (event) => {
+
+        event.preventDefault();
+        this.props._onLogin(this.state);
+
+    }
+
     render() {
         return (
             <div>
                 <input name="username" value={this.state.username} onChange={this.handleInput} />
                 <input name="password" type='password' value={this.state.password} onChange={this.handleInput} />
                 <input name="remember" type='checkbox' value={this.state.remember} onChange={this.handleInput} />
-                <button name="submit" type='submit' disabled={!this.state.username || !this.state.password} onClick={this.props._onLogin}>Login</button>
+                <button name="submit" type='submit' disabled={!this.state.username || !this.state.password} onClick={this.getFormData}>Login</button>
             </div>
         )
     }
