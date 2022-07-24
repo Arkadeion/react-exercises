f89f11fd817232c84176fcdefd600e3e30f71178
Arkadeion <arkadeion@gmail.com>
2022-07-24 17:17:41 +0200

    Fix 3

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,7 @@ export class Login extends React.Component {
     state = {
         username: '',
         password: '',
-        remember: 'false',
+        remember: false,
         loginButton: 'disabled'
     }
 
