97327f1da61e08dc1d645e615947dc852c9ba117
Arkadeion <arkadeion@gmail.com>
2022-08-09 16:19:06 +0200

    Add use-state-03

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 
 export class TodoList extends React.Component {
 
@@ -58,4 +58,55 @@ export class TodoList extends React.Component {
 
         )
     }
+} */
+
+import { useState } from "react"
+
+export function TodoList(props) {
+
+    const [items, setItems] = useState(['Go to work', 'Go to the gym', 'Clean up the house', 'Sleep'])
+
+    function addNewTodo(event) {
+
+        let newTodo = event.target.previousSibling.value;
+
+        setItems([
+            ...items,
+            newTodo,
+        ])
+
+        event.target.previousSibling.value = '';
+
+    }
+
+    function clearTodos() {
+        setItems([])
+    }
+
+    function removeTodo(event) {
+
+        let todoToDelete = event.target.parentElement.id;
+
+        let _items = [...items]
+
+        _items.splice(todoToDelete, 1);
+
+        setItems([
+            ..._items
+        ])
+
+    }
+
+    return (
+        <div>
+            <div>
+                <input className="border-black border-2 mr-3" name="newTodo" />
+                <button className="rounded-xl border-black border-2 p-1 mr-3" onClick={addNewTodo} >Add Item</button>
+                <button className="rounded-xl border-black border-2 p-1" onClick={clearTodos} >Clear All Todos</button>
+            </div>
+            <div>
+                {props.render(items, removeTodo)}
+            </div>
+        </div>
+    )
 }
