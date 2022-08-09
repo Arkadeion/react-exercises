4fa0661b02b8c2079b204c76820a9fb7a4c3e7ff
Arkadeion <arkadeion@gmail.com>
2022-08-09 18:21:51 +0200

    Add use-effect-02

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component {
@@ -30,4 +30,30 @@ Counter.defaultProps = {
     initialValue: 0,
     increaseValue: 1,
     interval: 1000,
+} */
+
+import { useEffect, useState } from "react";
+import { CounterDisplay } from "./CounterDisplay";
+
+export function Counter({ initialValue, increaseValue, interval }) {
+
+    const [count, setCount] = useState(initialValue);
+
+    useEffect(() => {
+        const countInterval = setInterval(() => {
+            setCount(count + increaseValue)
+        }, interval);
+
+        console.log(`The counter says ${count}`)
+
+        return () => {
+            clearInterval(countInterval);
+        }
+    })
+
+    return (
+        <div className="bg-white rounded-xl border-2 border-black max-w-fit p-4 mb-8">
+            <CounterDisplay count={count} />
+        </div>
+    )
 }
