304f90b8d4f3509a44be4916519c50de16c7308f
Arkadeion <arkadeion@gmail.com>
2022-08-10 15:46:40 +0200

    Add custom-hooks-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,41 +1,12 @@
-import { useEffect, useState } from "react"
+import { useGithubUser } from "./useGithubUser"
 
 export function GithubUser({ username }) {
 
-    const [user, setUser] = useState(null);
-
-    const [loading, setLoading] = useState(true);
-
-    async function fetchUser(username) {
-
-        setLoading(true);
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        const json = await response.json();
-
-        setLoading(false);
-        setUser(json);
-    }
-
-    useEffect(() => {
-        fetchUser(username)
-    }, [username]);
-
-/*     useEffect(() => {
-        setLoading(true);
-
-        fetch(`https://api.github.com/users/${username}`)
-            .then(response => {
-                return response.json()
-            })
-            .then((json) => {
-                setLoading(false);
-                setUser(json);
-                console.log(json);
-            })
-    }, [username]); */
+    const { user, loading, error } = useGithubUser({username})
 
     return <div className="bg-white rounded-xl border-2 border-black max-w-fit p-4 mb-8">
         {loading && <h1 className="text-xl font-bold" >Loading...</h1>}
         {user && <h1 className="text-xl font-bold" >{user.name}</h1>}
+        {error && <h1 className="text-xl font-bold" >{error.message}</h1>}
     </div>
 }
