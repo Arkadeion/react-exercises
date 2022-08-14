6a1579c8905e3066e32bde4eec43ac34ee3e1c44
Arkadeion <arkadeion@gmail.com>
2022-08-14 20:54:45 +0200

    Add swr-03

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -4,13 +4,18 @@ const fetcher = async (url) => fetch(url).then((response) => response.json())
 
 export function useGithubUser({username}) {
 
-    const { data, error } = useSWR(username !== null ? `https://api.github.com/users/${username}` : null, fetcher);
+    const { data, error, mutate } = useSWR(username !== null ? `https://api.github.com/users/${username}` : null, fetcher);
+
+    function handleDataRefresh() {
+        mutate(`https://api.github.com/users/${username}`);
+    }
 
     return (
         {
             user: data,
             isLoading: !data && !error,
             error: error,
+            handleDataRefresh: handleDataRefresh,
         }
     )
 }
