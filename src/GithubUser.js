685187bed1fd813a96b48da11bd076545d5d57e0
Arkadeion <arkadeion@gmail.com>
2022-08-14 19:02:18 +0200

    Add swr-01

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -5,11 +5,11 @@ export function GithubUser() {
 
     const { username } = useParams();
 
-    const { user, loading, error } = useGithubUser({username})
+    const { user, isLoading, error } = useGithubUser({username})
 
     return <div className="bg-white rounded-xl border-2 border-black max-w-fit p-4 mb-8">
-        {loading && <h1 className="text-xl font-bold" >Loading...</h1>}
-        {user && <h1 className="text-xl font-bold" >{user.name}</h1>}
+        {isLoading && <h1 className="text-xl font-bold" >Loading...</h1>}
         {error && <h1 className="text-xl font-bold" >{error.message}</h1>}
+        {!error && user && <h1 className="text-xl font-bold" >{user.name !== null ? user.name : `${user.login} has not set a name.`}</h1>}
     </div>
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,46 +1,16 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = async (url) => fetch(url).then((response) => response.json())
 
 export function useGithubUser({username}) {
-    const [user, setUser] = useState(null);
 
-    const [loading, setLoading] = useState(true);
-
-    const [error, setError] = useState(null);
-
-    async function fetchUser(username) {
-
-        setUser(null);
-        setLoading(true);
-        setError(null);
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        const json = await response.json();
-
-        if (response.status === 403) {
-            setError(new Error("You are being rate-limited by the Github API."));
-        } else if (response.status !== 200) {
-            setError(new Error("User not found."));
-        }
-
-        setLoading(false);
-
-        if (json.name !== null) {
-            setUser(json);
-        } else {
-            setError(new Error("User doesn't have a name."));
-        }
-
-    }
-
-    useEffect(() => {
-        fetchUser(username)
-    }, [username]);
+    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
 
     return (
         {
-            user: user,
-            loading: loading,
+            user: data,
+            isLoading: !data && !error,
             error: error,
-            fetchUser: fetchUser,
         }
     )
 }
