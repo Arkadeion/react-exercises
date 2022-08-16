f8befedd5b756ca43a36bfd48aa0f397a0141999
Arkadeion <arkadeion@gmail.com>
2022-08-16 16:53:44 +0200

    Fix function-components-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -10,54 +10,31 @@ import { LanguageContext } from "./LanguageContext";
 import { Login } from "./Login";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 
-export class Welcome extends React.Component {
+export function Welcome({ name, age }) {
 
-    state = {
-        language: 'en',
-    }
 
-    handleLanguageChange = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
-
-    onLogin = (data) => {
+    function onLogin(data) {
         console.log(data);
     }
 
-    render() {
-        return (
-            <div >
-                <div className="bg-white rounded-xl border border-red-700 p-3 mb-5 max-w-xs">
-                    <h1 className="text-2xl font-bold mb-4">Welcome, {this.props.name}!</h1>
-                    <Age age={this.props.age} />
-                </div>
-                <div>
-                    <div>
-                        <select value={this.state.language} onChange={this.handleLanguageChange}>
-                            <option value='en'>
-                                ENGLISH
-                            </option>
-                            <option value='it'>
-                                ITALIANO
-                            </option>
-                        </select>
-                    </div>
-                    <LanguageContext.Provider value={this.state.language}>
-                        <DisplayLanguage />
-                    </LanguageContext.Provider>
-                    <Counter initialValue={0} increaseValue={1} interval={1000} />
-                    <ClickCounter />
-                    <ClickTracker />
-                    <Login _onLogin={this.onLogin} />
-                    <UncontrolledLogin _onLogin={this.onLogin} />
-                    <Colors items={[{ id: 1, name: 'Green' }, { id: 2, name: 'Red' }, { id: 3, name: 'Blue' }, { id: 4, name: 'Black' }]} />
-                    <Container title="Todo List Container" />
-                </div></div>
+    return (
+        <div >
+            <div className="bg-white rounded-xl border border-red-700 p-3 mb-5 max-w-xs">
+                <h1 className="text-2xl font-bold mb-4">Welcome, {name}!</h1>
+                <Age age={age} />
+            </div>
+            <div>
+                <Counter initialValue={0} increaseValue={1} interval={1000} />
+                <ClickCounter />
+                <ClickTracker />
+                <Login _onLogin={onLogin} />
+                <UncontrolledLogin _onLogin={onLogin} />
+                <Colors items={[{ id: 1, name: 'Green' }, { id: 2, name: 'Red' }, { id: 3, name: 'Blue' }, { id: 4, name: 'Black' }]} />
+                <Container title="Todo List Container" />
+            </div>
+        </div>
 
-        )
-    }
+    )
 }
 
 
