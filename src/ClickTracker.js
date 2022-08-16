23ec80221fab7099747ce812c19a872eb529b712
Arkadeion <arkadeion@gmail.com>
2022-08-16 15:38:09 +0200

    Fix events-03

--- src/ClickTracker.js
+++ src/ClickTracker.js
@@ -7,16 +7,9 @@ export class ClickTracker extends React.Component {
     }
 
     trackLastButtonClicked = (event) => {
-        return this.setState((state) => {
-
-            if (event.target.id === 'button-wrapper') {
-
-                return;
-
-            } else {
-                return {
-                    lastButtonPressed: event.target.innerHTML
-                }
+        return this.setState(() => {
+            return {
+                lastButtonPressed: event.target.innerHTML
             }
         })
     }
@@ -25,10 +18,10 @@ export class ClickTracker extends React.Component {
         return (
             <div>
                 <h1>Last button pressed: {this.state.lastButtonPressed} </h1>
-                <div id='button-wrapper' onClick={this.trackLastButtonClicked}>
-                    <button>Button 1</button>
-                    <button>Button 2</button>
-                    <button>Button 3</button>
+                <div id='button-wrapper'>
+                    <button id='b1' onClick={this.trackLastButtonClicked}>Button 1</button>
+                    <button id='b2' onClick={this.trackLastButtonClicked}>Button 2</button>
+                    <button id='b3' onClick={this.trackLastButtonClicked}>Button 3</button>
                 </div>
             </div>
         )
